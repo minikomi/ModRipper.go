ee37a09229b33ad7b17105ebacd21a57bb6a28c2
Adam Moore <nerdfunk@gmail.com>
2012-06-18 12:19:32 +0900

    break out protracker code, in prep for xm

--- ModRipper.go
+++ ModRipper.go
@@ -50,26 +50,21 @@ func bufferFromFile(name string) (buffer *bytes.Buffer) {
 	return
 }
 
-type ProtrackerModule struct {
-	Title   string
-	Samples *[]Sample
-}
-
 type Sample struct {
 	Title  string
 	Length int
 	Data   []byte
 }
 
-type wavFile struct {
+type WavFile struct {
 	Filename     string
 	Header       *bytes.Buffer
 	FormatHeader *bytes.Buffer
 	Data         *bytes.Buffer
 }
 
-func NewWavFile() (wav *wavFile) {
-	wav = &wavFile{
+func NewWavFile() (wav *WavFile) {
+	wav = &WavFile{
 		Header:       bytes.NewBuffer(make([]byte, 0)),
 		FormatHeader: bytes.NewBuffer(make([]byte, 0)),
 		Data:         bytes.NewBuffer(make([]byte, 0)),
@@ -91,7 +86,7 @@ func NewWavFile() (wav *wavFile) {
 	return
 }
 
-func (w *wavFile) Construct(s Sample) {
+func (w *WavFile) Construct(s Sample) {
 	w.Filename = s.Title
 	w.Data.Write(bytesFromInt(len(s.Data)))
 	for _, byt := range s.Data {
@@ -101,7 +96,7 @@ func (w *wavFile) Construct(s Sample) {
 	w.Header.WriteString("WAVE")
 }
 
-func (w *wavFile) Dump() (err error) {
+func (w *WavFile) Dump() (err error) {
 	dump, err := os.OpenFile(w.Filename+".wav", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		return
@@ -137,6 +132,45 @@ func biggest(b []byte) (x byte) {
 	return x
 }
 
+func ProtrackerParse(rawData *bytes.Buffer) (samples []Sample) {
+
+	modTitle := string(bytes.Trim(rawData.Next(20), zerostring))
+	for i := 0; i < 31; i++ {
+		sampleTitle := modTitle + " - " + string(bytes.Trim(rawData.Next(22), zerostring))
+		sampleLengthBytes := rawData.Next(2)
+		var sampleLength uint16
+		buf := bytes.NewBuffer(sampleLengthBytes)
+		err := binary.Read(buf, binary.BigEndian, &sampleLength)
+		sampleLength = sampleLength * 2
+		if err != nil {
+			break
+		}
+		if sampleLength >= uint16(2) &&
+			len(sampleTitle) > 0 {
+			samples = append(samples, Sample{
+				Title:  sampleTitle,
+				Length: int(sampleLength),
+			})
+		}
+		//discard finetune (1 byte), volume (1 byte), repeat info (4 bytes)
+		_ = rawData.Next(6)
+	}
+
+	songLength := rawData.Next(2)[0] //discard unused 127 byte.
+	patternOrder := rawData.Next(133)[:songLength]
+	// discard pattern data
+	for i := 0; i < int(biggest(patternOrder))+1; i++ { // patterns start at 00, so add 1
+		_ = rawData.Next(1024)
+	}
+
+	for i, s := range samples {
+		samples[i].Data = rawData.Next(s.Length)
+	}
+
+	fmt.Println("Title:", modTitle, "Samples:", len(samples))
+	return
+}
+
 func main() {
 	flag.Parse()
 	if len(flag.Args()) == 0 {
@@ -144,42 +178,7 @@ func main() {
 	}
 	for _, f := range flag.Args() {
 		rawData := bufferFromFile(f)
-		modTitle := string(bytes.Trim(rawData.Next(20), zerostring))
-		samples := make([]Sample, 0)
-		for i := 0; i < 31; i++ {
-			sampleTitle := modTitle + " - " + string(bytes.Trim(rawData.Next(22), zerostring))
-			sampleLengthBytes := rawData.Next(2)
-			var sampleLength uint16
-			buf := bytes.NewBuffer(sampleLengthBytes)
-			err := binary.Read(buf, binary.BigEndian, &sampleLength)
-			sampleLength = sampleLength * 2
-			if err != nil {
-				break
-			}
-			if sampleLength >= uint16(2) &&
-				len(sampleTitle) > 0 {
-				samples = append(samples, Sample{
-					Title:  sampleTitle,
-					Length: int(sampleLength),
-				})
-			}
-			//discard finetune (1 byte), volume (1 byte), repeat info (4 bytes)
-			_ = rawData.Next(6)
-		}
-
-		songLength := rawData.Next(2)[0] //discard unused 127 byte.
-		patternOrder := rawData.Next(133)[:songLength]
-		// discard pattern data
-		for i := 0; i < int(biggest(patternOrder))+1; i++ { // patterns start at 00, so add 1
-			_ = rawData.Next(1024)
-		}
-
-		for i, s := range samples {
-			samples[i].Data = rawData.Next(s.Length)
-		}
-
-		fmt.Println("Title:", modTitle, "Samples:", len(samples))
-
+		samples := ProtrackerParse(rawData)
 		for _, s := range samples {
 			wav := NewWavFile()
 			wav.Construct(s)
